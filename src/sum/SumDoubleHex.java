ba181cc1759b269bdb60b42ad8f663e6eac6f78c
fedorilin750 <fiodoril@yandex.ru>
2026-02-20 01:55:24 +0300

    Добавлен SumDoubleHex

--- src/sum/SumDoubleHex.java
+++ src/sum/SumDoubleHex.java
@@ -3,20 +3,18 @@ package sum;
 public class SumDoubleHex {
     public static void main(String[] args) {
         double sum = 0.0;
-
         for (String arg : args) {
-            String[] parts = arg.split("\\s+");
-            for (String part : parts) {
+            for (String part : arg.split("\\s+")) {
                 if (part.isEmpty()) continue;
-
                 try {
                     sum += Double.parseDouble(part);
                 } catch (NumberFormatException e) {
-                    // игнор
+                    if (part.startsWith("0x")) {
+                        sum += Long.parseLong(part.substring(2), 16);
+                    }
                 }
             }
         }
-
         System.out.println(sum);
     }
 }
