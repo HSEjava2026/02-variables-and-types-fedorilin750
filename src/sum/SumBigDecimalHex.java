5ece43402fa39edc8c4d333668e9f59ce7ffa16e
fedorilin750 <fiodoril@yandex.ru>
2026-02-24 21:07:02 +0300

    Добавлена заготовка для SumBigDecimalHex

---
+++ src/sum/SumBigDecimalHex.java
@@ -0,0 +1,11 @@
+package sum;
+
+import java.math.BigDecimal;
+
+public class SumBigDecimalHex {
+    public static void main(String[] args) {
+        // TODO: реализовать суммирование hex-чисел с BigDecimal
+        // В разработке
+        System.out.println("0");
+    }
+}
